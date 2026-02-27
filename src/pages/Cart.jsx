c89d418848b76ed3f7d079cf6b7d9dc4375af4b1
abudardarajiya-eng <abudardarajiya@gmail.com>
2026-02-27 14:26:57 +0530

    college location

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,8 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 
 // --- RESTAURANT CONFIGURATION (Arrah, Bihar) ---
 const RESTAURANT_COORDS = {
-  lat: 28.5414837340927,   
-  lng: 77.33577886311099,
+  lat: 25.559080895405874,    
+  lng: 84.66458716857322,
 };
 const MAX_DISTANCE_METERS = 100; // Allowed radius around the restaurant
 
